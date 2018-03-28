fd7ca67783f1155aef5e94cbbc27156e0e5520a2
vrandkode <vrandkode@gmail.com>
2018-03-28 13:32:53 +0200

    Update copy.py

--- blog/cassandra/copy.py
+++ blog/cassandra/copy.py
@@ -8,8 +8,7 @@ con = MySQLdb.connect("127.0.0.1","root","toor","example" )
 with con:
   print('connecting to database')
   cur = con.cursor()
-  cur.execute("COPY incidents(id, category, description, dayoftheweek, time, district, resolution, address,x,y,location) FROM \'%s\' 
-WITH DELIMITER=\'\\t\' and HEADER=false and DATETIMEFORMAT=\'%%Y-%%m-%%d %%H:%%M:%%S\';", (filename))
+  cur.execute("COPY incidents(id, category, description, dayoftheweek, time, district, resolution, address,x,y,location) FROM \'%s\' WITH DELIMITER=\'\\t\' and HEADER=false and DATETIMEFORMAT=\'%%Y-%%m-%%d %%H:%%M:%%S\';", (filename))
   cur.close()
   con.commit()
 con.close()
